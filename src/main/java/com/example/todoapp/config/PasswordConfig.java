cca46bd51b11bccb92a497316016c95adfd34e1b
dmlal <opkl8781@naver.com>
2023-11-18 12:14:21 +0900

    feat : 비밀번호 암호화 메소드,  회원가입 클래스 생성 및 정규화 적용

--- src/main/java/com/example/todoapp/config/PasswordConfig.java
+++ src/main/java/com/example/todoapp/config/PasswordConfig.java
@@ -1,13 +1,12 @@
 package com.example.todoapp.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class Config {
+public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/example/todoapp/controller/SignupController.java
+++ src/main/java/com/example/todoapp/controller/SignupController.java
@@ -1,8 +1,13 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.dto.SignupRequestDto;
 import com.example.todoapp.service.SignupService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,6 +15,10 @@ public class SignupController {
 
     private final SignupService signupService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
+        return null;
+    }
 
 
 }
